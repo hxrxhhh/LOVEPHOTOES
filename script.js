d6c2a3f949ae333479ec8ed33931bd0fd0915441
hxrxhhh <shadowbuster371@gmail.com>
2025-04-05 04:55:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
+const proceedBtn = document.getElementById('proceedBtn');
 const message = document.getElementById('message');
 const jumpscareVideo = document.getElementById('jumpscareVideo');
 
-setTimeout(() => {
+proceedBtn.addEventListener('click', () => {
+  proceedBtn.style.display = "none"; // Hide button
+  message.style.display = "block";
   message.textContent = "Ready";
-}, 2000);
+});
 
-setTimeout(() => {
-  message.textContent = "You really want it huh!";
-}, 4000);
-
-setTimeout(() => {
-  message.style.display = "none";
-  jumpscareVideo.style.display = "block";
-  jumpscareVideo.play();
-}, 6000);
+message.addEventListener('click', () => {
+  if (message.textContent === "Ready") {
+    message.textContent = "You really want it huh!";
+    
+    setTimeout(() => {
+      message.style.display = "none";
+      jumpscareVideo.style.display = "block";
+      jumpscareVideo.play();
+    }, 2000);
+  }
+});
 
 // When video ends, show April Fool message
 jumpscareVideo.addEventListener('ended', () => {
